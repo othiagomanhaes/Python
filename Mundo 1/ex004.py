c5457b69e6f0079a94e952dceeeaffc21b56824f
Thiago Manhães <thiagomanhaes2@gmail.com>
2022-01-10 22:59:19 -0300

    Primeiros exercícios do mundo 1

---
+++ Mundo 1/ex004.py
@@ -0,0 +1,8 @@
+a =input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaços?', a.isspace())
+print('É um número?', a.isnumeric())
+print('É do alfabeto?', a.isalpha())
+print('Tem letras e números?', a.isalnum())
+print('Está com maiúsculas?', a.isupper())
+print('Está capitalizada?', a.istitle())
