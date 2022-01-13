5bcc615ab02f4460d2e6319eef53c59709efc2ea
Thiago Manhães <thiagomanhaes2@gmail.com>
2022-01-13 15:29:53 -0300

    ultima mudança

--- Mundo 1/ex10.py
+++ Mundo 1/ex10.py
@@ -1,3 +1,3 @@
-c = float(input('Qual o valor total em sua carteira? R$'))
-d = c / 5.56
+r = float(input('Qual o valor total em sua carteira? R$'))
+d = r / 5.56
 print('Você pode comprar com esse valor {:.2f} doláres.'.format(d))
